481ed9bc7ba18677fe5b2b1170a0700a65482ec1
Arik Alexandrov <arikxl@gmail.com>
2021-11-14 02:29:41 +0200

    options

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, forwardRef } from 'react';
 import MaterialTable from 'material-table';
 
-import {students} from './data/students';
-import {columns} from './data/columns';
+
+
+
+
+import { students } from './data/students';
+import { columns } from './data/columns';
+import { tableIcons } from './data/tableIcons';
 
 const Home = () => {
 
     const [tableData, setTableData] = useState(students)
-
-
 
     return (
         <div>
@@ -17,6 +20,17 @@ const Home = () => {
             <MaterialTable columns={columns}
                 data={tableData}
                 title="Students Info"
+                options={{
+                    searchAutoFocus: true,
+                    filtering: true,
+                    pageSize: 10,
+                    paginationPosition: 'both',
+                    exportButton: true,
+                    exportAllData: true,
+                    exportFileName:'Soaps',
+                    
+                }}
+                icons={tableIcons}
             />
         </div>
     )
--- src/components/data/columns.js
+++ src/components/data/columns.js
@@ -1,8 +1,10 @@
 export const columns = [
     { title: "Name", field: "name" },
-    { title: "Email", field: "email" },
+    { title: "Email", field: "email", filterPlaceholder: "Filter" },
     { title: "Phone Number", field: "phone" },
     { title: "Age", field: "age" },
-    { title: "Gender", field: "gender" },
+    { title: "Gender", field: "gender", lookup:{M:'Male',F:'Female'} },
     { title: "City", field: "city" },
+    { title: "School Fee", field: "fee", type: 'currency',
+     currencySetting:{currencyCode:'NIS', minimumFractionDigits:0} },
 ]
